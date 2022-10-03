d042048977b404814fc3f2c5344f499acc19620d
sergimol <sergimol@ucm.es>
2022-10-03 19:21:51 +0200

    Cosas del board

--- Practica1/Logic/src/main/java/com/example/logic/Board.java
+++ Practica1/Logic/src/main/java/com/example/logic/Board.java
@@ -1,5 +1,34 @@
 package com.example.logic;
 
-public class Board {
+import java.util.Random;
+import java.util.Vector;
 
+public class Board {
+    int xSize, ySize;
+    Cell cells[][];
+
+    Cell getCell(int x, int y){ return cells[x][y]; }
+    Vector<Vector<Integer>> xValues;
+
+    Board(int x, int y){
+        xSize = x;
+        ySize = y;
+        cells = new Cell[xSize][ySize];
+
+        boolean good = false;
+        Random rd = new Random();
+        int consecutives = 0;
+        for(int i = 0; 0 < xSize; i++){
+            for(int j = 0; 0 < ySize; j++){
+                good = rd.nextBoolean();
+                cells[i][j] = new Cell(x, y, good);
+                if(good)
+                    consecutives++;
+                else{
+                    xValues.elementAt(i).add(consecutives);
+                    consecutives = 0;
+                }
+            }
+        }
+    }
 }
--- Practica1/Logic/src/main/java/com/example/logic/Cell.java
+++ Practica1/Logic/src/main/java/com/example/logic/Cell.java
@@ -4,9 +4,18 @@ enum States{
     Grey, Blue, Empty, Red
 }
 public class Cell {
-    int x, y;
+    // Posiciones logicas dentro del tablero
+    int xPos, yPos;
+    // Estado de la casilla
     States state = States.Grey;
+    // True = casilla forma parte del dibujo, False = no forma parte
+    boolean isGood;
 
+    Cell(int x, int y, boolean good){
+        xPos = x;
+        yPos = y;
+        isGood = good;
+    }
     States getState() { return state; }
     void changeState(){
         state = States.values()[state.ordinal() + 1];
