cc5222339a44993913c40f5a0aa6ab0ba9ee39bb
diecasti <diecasti@ucm.es>
2022-11-06 12:33:21 +0100

    input list checked

--- Practica1/JavaEngine/src/main/java/com/example/engine/JInput.java
+++ Practica1/JavaEngine/src/main/java/com/example/engine/JInput.java
@@ -4,6 +4,7 @@ package com.example.engine;
 
 import com.example.interfaces.IInput;
 
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList; //<----------- podemos usarlo¿?
 
 
@@ -13,7 +14,7 @@ import javax.swing.JFrame;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 
-public class JInput implements IInput, MouseListener  {
+public class JInput implements IInput, MouseListener, MouseMotionListener {
 
 
     private List<TouchEvent> eventList;
@@ -22,6 +23,7 @@ public class JInput implements IInput, MouseListener  {
     public JInput(JFrame frame) {
         //Register for mouse events on blankArea and the panel.
         frame.addMouseListener(this);
+        frame.addMouseMotionListener(this);
         eventList = new ArrayList<TouchEvent>();
     }
 
@@ -59,10 +61,28 @@ public class JInput implements IInput, MouseListener  {
         e.consume();
 
         eventOutput("Mouse pressed (# of clicks: "
-                + e.getClickCount() + ")", e);
+                + e.getClickCount() + " position: " +e.getX() + " " + e.getY() + " )", e);
     }
 
     public void mouseReleased(MouseEvent e) {
+
+        //repetir esta estructura con el resto de eventos que queramos registrar
+        Event evento = new Event();
+        evento.x = e.getX();
+        evento.y = e.getY();
+        //evento.x = e.getXOnScreen();
+        //evento.y = e.getYOnScreen();
+        evento.type = InputTouchType.TOUCH_UP;
+        evento.index = e.getID();
+
+        //añadir el evento a la lista de cosas procesadas
+        eventList.add(evento);
+
+        //podemos consumir el evento para que en el caso de que el canvas sea hijo de algun otro objeto, se termine aqui la propagacion de este y el padre no tenga que vovler a procesar este input (evitando asi que se registre otra vez el input)
+        //ejemplo de esot es querer registrar el pulsado de un cubo que es  hijo de el canvas con el resto de cubois, de formar que solo el cubo clickado proceasa el input)ç
+        e.consume();
+
+
         eventOutput("Mouse released (# of clicks: "
                 + e.getClickCount() + ")", e);
     }
@@ -78,5 +98,36 @@ public class JInput implements IInput, MouseListener  {
     public void mouseClicked(MouseEvent e) {
         eventOutput("Mouse clicked (# of clicks: "
                 + e.getClickCount() + ")", e);
+    }
+
+
+    //movimiento del raton
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        //repetir esta estructura con el resto de eventos que queramos registrar
+        Event evento = new Event();
+        evento.x = e.getX();
+        evento.y = e.getY();
+        //evento.x = e.getXOnScreen();
+        //evento.y = e.getYOnScreen();
+        evento.type = InputTouchType.TOUCH_MOVE;
+        evento.index = e.getID();
+
+        //añadir el evento a la lista de cosas procesadas
+        eventList.add(evento);
+
+        //podemos consumir el evento para que en el caso de que el canvas sea hijo de algun otro objeto, se termine aqui la propagacion de este y el padre no tenga que vovler a procesar este input (evitando asi que se registre otra vez el input)
+        //ejemplo de esot es querer registrar el pulsado de un cubo que es  hijo de el canvas con el resto de cubois, de formar que solo el cubo clickado proceasa el input)ç
+        e.consume();
+
+
+        eventOutput("Mouse pressed moved (button: "
+                + e.getButton() + " position: " +e.getX() + " " + e.getY() + " )", e);
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
     }
 }
